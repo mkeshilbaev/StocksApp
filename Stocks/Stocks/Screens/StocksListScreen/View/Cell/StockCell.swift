c90b3305de1dfb193a85a0e1cf88428a3bf9e069
Madi Keshilbayev <m.keshilbaev@MacBook-Pro-Madi-2.local>
2022-06-06 12:55:21 +0600

    refactor

--- Stocks/Stocks/Screens/StocksListScreen/View/Cell/StockCell.swift
+++ Stocks/Stocks/Screens/StocksListScreen/View/Cell/StockCell.swift
@@ -64,7 +64,7 @@ final class StockCell: UITableViewCell {
 	}
 
 	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+		fatalError("init(coder:) has not been implemented...")
 	}
 
 	@objc func favouriteButtonTap(){
